8c339a1a4b81603f45269979bf002e376a86695f
3thann <90375627+3thann@users.noreply.github.com>
2022-10-18 13:04:24 +0200

    Finish first step - v0.0.2

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const nom = document.querySelector("#nom");
 const prenom = document.querySelector("#prenom");
 const formulaire = document.querySelector("#formulaire");
 
-formulaire.addEventListener("submit", function(e){
-    document.getElementById("table").innerHTML = "<tr><td>" + nom.value + "</td><td>" + prenom.value + "</td></tr>"
+formulaire.addEventListener("submit", function(e) {
     e.preventDefault();
+    addRow(document.getElementById('table'), nom, prenom);
+    nom.value = ""
+    prenom.value = ""
     console.log("toto");
-}
-);
+});
 
+function addRow(elmt, nom, prenom) {
+    let tr = document.createElement('tr');
+    elmt.appendChild(tr);
+     
+    let tdNom = document.createElement('td');
+    tr.appendChild(tdNom);
+    let tdNomTxt = document.createTextNode(nom.value);
+    tdNom.appendChild(tdNomTxt);
+    let tdPrenom = document.createElement('td');
+    tr.appendChild(tdPrenom);
+    let tdPrenomTxt = document.createTextNode(prenom.value);
+    tdPrenom.appendChild(tdPrenomTxt);
+}
